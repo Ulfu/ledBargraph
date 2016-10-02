89719082b12213fc15677dbcd7931cf3e6296044
Ulfu <din_dr_elvis@live.se>
2016-10-02 15:49:04 +0200

    New version

--- ledBargraphArray/ledBargraphArray.ino
+++ ledBargraphArray/ledBargraphArray.ino
@@ -21,7 +21,6 @@ const int buttonPin2 = 3;      // the number of the button pin
 
 int delayTime = 200;
 int ledsState[8]; //Current LEDs that are HIGH
-int ledsOn = 0;
 boolean buttonValue1;
 boolean buttonValue2;
 
@@ -43,25 +42,21 @@ void setup() {
 void loop() {
   buttonValue1 = digitalRead(buttonPin1);
   buttonValue2 = digitalRead(buttonPin2);
-  if (!buttonValue1) {    
-    ledsOn++;
-    delay(delayTime);    
-  }
-  if (!buttonValue2) {
-    ledsOn--;
-    delay(delayTime);
-  }
-  ledsOn = constrain(ledsOn, 0, 8);
+ 
   for (int i = 0;  i < 8; i++) {
-    if (ledsOn > i) {
+    if (!ledsState[i] && !buttonValue1) {
       ledsState[i] = true;
     }
-    else {
+    if  (ledsState[i] && !buttonValue2) {
       ledsState[i] = false;
     }
     digitalWrite(ledPins[i], ledsState[i]);
+    if  (!buttonValue1 || !buttonValue1) {
+     Serial.println(ledsState[i]); //Prints led state
+     delay(delayTime);
+     break;
+    }
   }
-  Serial.println(ledsOn); //
 }
 
 
