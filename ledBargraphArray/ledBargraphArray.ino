707277d12c0e6feca1bb791a2907b7b033da119e
Kristian <din_dr_elvis@live.se>
2016-10-05 13:37:04 +0200

    Fixed

--- ledBargraphArray/ledBargraphArray.ino
+++ ledBargraphArray/ledBargraphArray.ino
@@ -1,17 +1,6 @@
 /*
-
-
-
- The circuit:
- * LED attached from pin 3-13 to ground
- * pushbutton attached to pin 2 Pullupp-switch
- * pushbutton attached to pin 3 Pullupp-switch
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
- This example code is in the public domain.
- */
+  This example code is in the public domain.
+*/
 
 // constants won't change. They're used here to
 // set pin numbers:
@@ -23,14 +12,15 @@ int delayTime = 200;
 int ledsState[8]; //Current LEDs that are HIGH
 boolean buttonValue1;
 boolean buttonValue2;
+boolean LEDchanged = false;
 
 void setup() {
   for (int i = 0; i < 8; i++) {
-  //Assign number of pins
-    ledPins[i] = i + 4; 
-  //Set all led states to LOW 
+    //Assign number of pins
+    ledPins[i] = i + 4;
+    //Set all led states to LOW
     ledsState[i] = false;
-  // initialize the LED pins as an outputs:
+    // initialize the LED pins as an outputs:
     pinMode(ledPins[i], OUTPUT);
   }
   // initialize the button pins as an input pullup:
@@ -42,21 +32,26 @@ void setup() {
 void loop() {
   buttonValue1 = digitalRead(buttonPin1);
   buttonValue2 = digitalRead(buttonPin2);
- 
+
   for (int i = 0;  i < 8; i++) {
+
+    Serial.println(ledsState[i]); //Prints led state
     if (!ledsState[i] && !buttonValue1) {
       ledsState[i] = true;
-    }
-    if  (ledsState[i] && !buttonValue2) {
-      ledsState[i] = false;
-    }
-    digitalWrite(ledPins[i], ledsState[i]);
-    if  (!buttonValue1 || !buttonValue1) {
-     Serial.println(ledsState[i]); //Prints led state
-     delay(delayTime);
-     break;
+      
+      digitalWrite(ledPins[i], ledsState[i]);
+      delay(delayTime);
+      break;
     }
   }
+   for (int i = 7;  i >= 0 ; i--) {
+      if  (ledsState[i] && !buttonValue2) {
+      ledsState[i] = false;
+      LEDchanged = true;
+
+      digitalWrite(ledPins[i], ledsState[i]);
+      delay(delayTime);
+      break;
+    }
+   }
 }
-
-
